fd482303e7e65c0077221eec87ce7342d8fac3e8
Caio Jacobina <caiosantanaj@gmail.com>
2023-11-12 23:50:10 +0000

    computed properties

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      selectedVariant: 0,
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+          onSale: true,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+          onSale: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+    getTitle(isOnSale) {
+      const brand = this.brand;
+      const product = this.product;
+
+      return `${brand} ${product}${isOnSale ? " - on sale" : ""}`;
+    },
+  },
+  computed: {
+    title() {
+      return this.getTitle(this.variants[this.selectedVariant].onSale);
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity > 0;
+    },
+  },
+});
